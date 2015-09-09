14a7222531fde753b57bb13810b61df5b6a7efae
Louy <me@l0uy.com>
2015-09-09 15:50:58 +0300

    Project setup

---
+++ test/index.js
@@ -0,0 +1,12 @@
+/* jshint mocha: true, expr: true */
+
+var chai = require('chai'),
+    expect = chai.expect,
+    sinonChai = require('sinon-chai'),
+    sinon = require('sinon'),
+    rtler = require('..');
+
+chai.use(sinonChai);
+
+describe('rtler', function() {
+});
