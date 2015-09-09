a22c6be0274d6de5300c6da0d92c78ee4d16d479
Louy <me@l0uy.com>
2015-09-09 16:05:28 +0300

    Remove sinon

--- test/index.js
+++ test/index.js
@@ -2,11 +2,7 @@
 
 var chai = require('chai'),
     expect = chai.expect,
-    sinonChai = require('sinon-chai'),
-    sinon = require('sinon'),
     rtler = require('..');
-
-chai.use(sinonChai);
 
 describe('rtler', function() {
 });
