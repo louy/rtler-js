230a24ef65d11ec3aa6663522bbe46d124eca2cc
Louy <me@l0uy.com>
2015-09-09 19:28:16 +0300

    Some functionality

--- rtler.js
+++ rtler.js
@@ -1,18 +1,71 @@
-var SWITCHABLE_AUTO = {
-  'right': 'left',
-  'margin': 'margin',
-};
-var SWITCHABLE_ZERO = {
-  'padding': 'padding',
-};
+var css = require('css');
 
-function removeComments(css) {
-  return css.replace(/\/\*.*\*\//g,'');
+var SWITCHABLE_AUTO = [
+  'right',
+  'left',
+  'margin',
+  'margin-right',
+  'margin-left',
+];
+var SWITCHABLE_ZERO = [
+  'padding',
+  'padding-right',
+  'padding-left',
+];
+
+function switchPropertyName(property) {
+  return property
+                .replace('right', 'RIGHT')
+                .replace('left', 'right')
+                .replace('RIGHT', 'left');
 }
 
-function flip(css) {
-  var flipped = removeComments(css);
-  return flipped;
+function flip(cssText) {
+  var options = { compress: true };
+
+  var json = css.parse(cssText, options);
+
+  json.stylesheet.rules.forEach(function(rule) {
+    var declarations = [], properties = [];
+
+    rule.declarations.forEach(function(declaration) {
+      if (SWITCHABLE_AUTO.indexOf(declaration.property) > -1 ||
+          SWITCHABLE_ZERO.indexOf(declaration.property) > -1) {
+        declaration.property = switchPropertyName(declaration.property);
+        properties.push(declaration.property);
+        declarations.push(declaration);
+      }
+    });
+
+    declarations.forEach(function(declaration) {
+      var opposite = switchPropertyName(declaration.property);
+      if (properties.indexOf(opposite) < 0) {
+        if (SWITCHABLE_AUTO.indexOf(opposite) > -1) {
+          declarations.push({
+            type: 'declaration',
+            property: opposite,
+            value: 'auto',
+          });
+          properties.push(opposite);
+        } else if (SWITCHABLE_ZERO.indexOf(opposite) > -1) {
+          declarations.push({
+            type: 'declaration',
+            property: opposite,
+            value: '0',
+          });
+          properties.push(opposite);
+        }
+      }
+    });
+
+    rule.declarations = declarations;
+  });
+
+  json.stylesheet.rules = json.stylesheet.rules.filter(function(rule) {
+    return rule.declarations.length > 0;
+  });
+
+  return css.stringify(json, options);
 }
 
 function override(css) {
--- test/index.js
+++ test/index.js
@@ -12,12 +12,21 @@ describe('rtler', function() {
   });
 
   describe('#flip', function() {
-    it('removes comments', function() {
-      var a = '/*abc*/', b = '';
+    it('skips empty declarations', function() {
+      var a = 'div{}', b = '';
       expect(rtler.flip(a)).to.equal(b);
     });
-    it('flips', function() {
-      var a = 'div{right:0}', b = 'div{left:0}';
+    it('flips with auto', function() {
+      var a = 'div{right:0;}', b = 'div{left:0;right:auto;}';
+      expect(rtler.flip(a)).to.equal(b);
+    });
+    it('flips margin', function() {
+      var a = 'div{margin-right:0;}', b = 'div{margin-left:0;margin-right:auto;}';
+      expect(rtler.flip(a)).to.equal(b);
+    });
+    it('flips with 0', function() {
+      var a = 'div{padding-right:10px;}',
+          b = 'div{padding-left:10px;padding-right:0;}';
       expect(rtler.flip(a)).to.equal(b);
     });
   });
