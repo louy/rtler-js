a09337ad577ec703c0242d43921514571a12c894
Louy <me@l0uy.com>
2015-09-09 16:10:29 +0300

    Simple test cases

--- test/index.js
+++ test/index.js
@@ -5,4 +5,27 @@ var chai = require('chai'),
     rtler = require('..');
 
 describe('rtler', function() {
+  it('is as expected', function() {
+    expect(rtler).to.be.an('object');
+    expect(rtler).itself.to.respondTo('flip');
+    expect(rtler).itself.to.respondTo('override');
+  });
+
+  describe('#flip', function() {
+    it('removes comments', function() {
+      var a = '/*abc*/', b = '';
+      expect(rtler.flip(a)).to.equal(b);
+    });
+    it('flips', function() {
+      var a = 'div{right:0}', b = 'div{left:0}';
+      expect(rtler.flip(a)).to.equal(b);
+    });
+  });
+
+  describe.skip('#override', function() {
+    it('creates override', function() {
+      var a = 'div{right:0}', b = 'div{left:0;right:auto}';
+      expect(rtler.override(a)).to.equal(b);
+    });
+  });
 });
